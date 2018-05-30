675475367401d9481f8d89e40f8107530263d1db
Stas <vahnenkostas@mail.ru>
2018-05-30 18:20:23 +0600

    5 commit

---
+++ lesson 1/hard/hard.js
@@ -0,0 +1,16 @@
+var num = mas(33721),
+	num2;
+
+function mas(n) {
+  n = String(n).split('').map(Number);
+  return n;
+}
+  num2 = (num[0] * num[1] * num[2] * num[3] * num[4]);
+  console.log(num2);
+  var over = mas2(num2 ** 3);
+  
+  function mas2(m) {
+  m = String(m).split('').map(Number);
+  return m;
+}
+ document.write(over[0], over[1]);
