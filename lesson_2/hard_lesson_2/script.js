b381cd37571ea44f8f09d7f0722548f12833cb8c
Stas <vahnenkostas@mail.ru>
2018-05-31 20:55:01 +0600

    8 commit

---
+++ lesson_2/hard_lesson_2/script.js
@@ -0,0 +1,11 @@
+var week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+	content = (week[0]  + '</br>' + week[1] + '</br>' + week[2] + '</br>' + week[3].italics() + '</br>' + week[4] + '</br>' + week[5].bold() + '</br>' + week[6].bold()),
+	arr = ["4256", "322", "235125", "3552", "75674", "456", "733"];
+
+document.write(content);//Выводит месяцы
+
+for (var i = 0; i < arr.length; i++) {
+	if (arr[i].charAt(0) == 3 || arr[i].charAt(0) == 7) {
+		console.log(arr[i])
+	}
+}//Выводит в консоль числа массива ничинающиеся с 3 и 7
---
+++ lesson_2/main.js
@@ -0,0 +1,56 @@
+var budgetMonth = prompt("Ваш бюджет за месяц?", "Введите число"),
+	yourNameShop = prompt("Название вашего магазина?","Ваше название");
+
+var mainList = {
+	budget: budgetMonth,
+	shopName: yourNameShop,
+	shopGoods: [],
+	employers: {},
+	open: false
+};
+
+/*var i = 0;
+while(i < 5) {
+	console.log(i);
+	i++;
+	var product = prompt("Какой тип товаров будем продавать?");
+ 		mainList.shopGoods[i] = product;
+ 	
+}; 
+
+if ((typeof(product)) === 'string' && (typeof(product)) != null && product != '' && product.length < 50) {
+ 		console.log('Все верно');
+ 	} else {
+ 		console.log("Вы где то ошиблись")
+ 	}*/
+ 	//Первый способ	 !!
+
+ 	/*var i = 0;
+ 	do {
+		console.log(i);
+		i++;
+		var product = prompt("Какой тип товаров будем продавать?");
+		mainList.shopGoods[i] = product;
+ 	} while(i < 5);
+if ((typeof(product)) === 'string' && (typeof(product)) != null && product != '' && product.length < 50) {
+ 		console.log('Все верно');
+ 	} else {
+ 		console.log("Вы где то ошиблись")
+ 	}*/
+ 	// Второй способ !!
+ 	
+
+
+ for (let i = 0; i < 5; i++) {
+ 	let product = prompt("Какой тип товаров будем продавать?");
+ 	if ((typeof(product)) === 'string' && (typeof(product)) != null && product != '' && product.length < 50) {
+ 		console.log('Все верно');
+ 		mainList.shopGoods[i] = product;
+ 	} else {
+		console.log("Вы где то ошиблись")
+ 	}
+ }
+
+
+console.log(mainList);
+alert("Ваш бюджет на 1 день : " + mainList.budget / 30);
