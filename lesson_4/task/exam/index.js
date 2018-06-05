9d03649cbe20430028a4215bcbd69c4d41a2bc21
Stas <vahnenkostas@mail.ru>
2018-06-05 22:40:56 +0600

    14 commit

--- lesson_4/task/exam/index.js
+++ lesson_4/task/exam/index.js
@@ -1,42 +1,30 @@
-function getFriendlyNumbers(start, end) {
-	if (start > end || start < 0 || (typeof(start)) != 'number' || (typeof(end)) != 'number' || start % 1 != 0 || end % 1 != 0) {
-		return false;
+function getFriendlyNumbers(start,end){
+	if((typeof(start)) != 'number' || (typeof(end)) != 'number' || start > end || end < 0 || start < 0 || start % 1 != 0 || end % 1 != 0) {
+		return false
 	}
-
-	var dividers = [],
-		obj = {
-			num1:{
-				num:[],
-				sum:[]
-			},
-			num2:{
-				num:[],
-				sum:[]
-			}
-		};
-		for(let i = start; i <= end; i++){
-			obj.num1.num.push(i);
-			sum = 0;
-			for (let j = 1; j <= i; j++) {
-				if( i % j == 0) {
-					sum += j;
-					obj.num1.sum.push(j);
-				}
-			}
-		}return obj
-		console.log(obj)
-
-	function getNumbers() {
-		if (obj.num1 == obj.sum2 && obj.num2 == obj.sum1) {
-			dividers.push(num1,num2);
-		}
-	}
-    return [dividers];
+  var pairs = {}, i, j, result = []
+  for (i = start; i <= end; i++){
+    var sum = 0
+    for(j = 1; j < i; j++){
+      if( i % j == 0){
+        sum += j
+      }
+    }
+    pairs[i] = sum
+  }
+  for(i in pairs){
+    for(j in pairs){
+      if( j == i) continue
+      if( pairs[i] == j && i == pairs[j] && i > pairs[i]) {
+        result.push([pairs[j],pairs[i]].sort())
+      }
+    }
+  }
+  return result
 }
-console.log(getFriendlyNumbers(100, 200));
 
-/*module.exports = {
+module.exports = {
     firstName: 'Name',
     secondName: 'Surname',
     task: getFriendlyNumbers
-}*/
+}
